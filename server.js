fcff3924412b9668bcba8086618cc60dc424728d
Stranger0107 <pratham.masurkar23@spit.ac.in>
2025-08-28 22:10:19 +0530

    server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.json());
 // Connect MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.log("❌ DB Error:", err));
+  .then(() => console.log(" MongoDB Connected"))
+  .catch((err) => console.log(" DB Error:", err));
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
@@ -24,4 +24,4 @@ app.use('/api/admin', adminRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
